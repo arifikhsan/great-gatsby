78a0fd7238876459fd4d4f388128936692bdb471
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-08 12:45:32 +0700

    add internal link

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 export default () => {
   return (
     <div>
       <h1>About</h1>
       <p>I like to play games!</p>
+      <Link to='/contact'>Contact me</Link>
     </div>
   )
 }
