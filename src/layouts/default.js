c54fd3098d6ac911fa13ebdce7083a50905a0aa5
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-08 17:06:44 +0700

    example styles

--- src/layouts/default.js
+++ src/layouts/default.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import Header from "../components/header"
 import Footer from "../components/footer"
+import '../styles/index.css'
 
 export default props => {
   return (
