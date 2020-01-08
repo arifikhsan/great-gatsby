125cf01e373074a4b0271d5f1fdd8a4a098563f1
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-08 17:06:31 +0700

    a to Link

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 import Layout from "../layouts/default"
+import { Link } from "gatsby"
 
 export default () => {
   return (
@@ -8,9 +9,12 @@ export default () => {
       <h1>Contact</h1>
       <p>
         The best way to reach me is via{" "}
-        <a href="https://www.github.com/arifikhsan/" target="_blank">
+        <Link
+          href="https://www.github.com/arifikhsan/"
+          target="_blank"
+        >
           Arif Ikhsanudin
-        </a>{" "}
+        </Link>{" "}
         on github!
       </p>
     </Layout>
