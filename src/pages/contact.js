b7ca579a9fd7abc63356d6e1c0cf54fa3af6f115
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-08 12:45:15 +0700

    add external link

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,11 @@ export default () => {
   return (
     <div>
       <h1>Contact</h1>
-      <p>The best way to reach me is via @arif.ikhsanudin.id on email!</p>
+      <p>
+        The best way to reach me is via{" "}
+        <a href="https://www.github.com/arifikhsan/">Arif Ikhsanudin</a> on
+        github!
+      </p>
     </div>
   )
 }
