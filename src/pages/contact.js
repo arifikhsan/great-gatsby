562ac7e5431ecfa2713b66669bbc2c867aa4f3b8
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-01-08 12:47:34 +0700

    open in new tab

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,7 +6,7 @@ export default () => {
       <h1>Contact</h1>
       <p>
         The best way to reach me is via{" "}
-        <a href="https://www.github.com/arifikhsan/">Arif Ikhsanudin</a> on
+        <a href="https://www.github.com/arifikhsan/" target="_blank">Arif Ikhsanudin</a> on
         github!
       </p>
     </div>
